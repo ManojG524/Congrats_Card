19ff28cb01d1ad78f667368fe590549412f19b6f
ManojG524 <m30843003@gmail.com>
2022-12-06 15:12:50 +0000

    congrats Card

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  // Write your code here.
+  <div className="bgImage">
+    <h1>Congratulations</h1>
+    <div className="boxContainer">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>
+        Want to quickly review some of the concepts you’ve been learning? Take a
+        look at the Cheat Sheets.
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
